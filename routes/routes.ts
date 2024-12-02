c84ae58e9faf1b5e258deb442dd562b363365d5c
Mabo Rustam <kimlael01@mail.ru>
2024-12-02 10:25:56 +0600

    fileDbMethod.ts created

---
+++ routes/routes.ts
@@ -0,0 +1,16 @@
+import express from "express";
+
+
+const routerRoutes = express.Router();
+
+routerRoutes.get("/", async (req, res) => {
+
+})
+routerRoutes.get("/:id", async (req, res) => {
+
+})
+routerRoutes.post("/", async (req, res) => {
+
+});
+
+export default routerRoutes;
